ddee4a4c48e9e083f8bac11babb81522b8cdd10e
BieniuCieniu <mikibien@outlook.com>
2022-11-11 20:59:46 +0100

    simpler export for discord commands

---
+++ apps/bot/src/app/commands/slashCommands/index.ts
@@ -0,0 +1,7 @@
+import { getQueue } from "./getQueue";
+import { pause } from "./pause";
+import { play } from "./play";
+import { skip } from "./skip";
+import { stop } from "./stop";
+
+export default [play, pause, stop, skip, getQueue];
