8b152d7146edc19c3f0d919a43c1a264751b6ab7
BieniuCieniu <mikibien@outlook.com>
2022-11-14 15:27:29 +0100

    fix for api not returning proper data

--- libs/fnc/src/lib/getYTvData.ts
+++ libs/fnc/src/lib/getYTvData.ts
@@ -3,25 +3,29 @@ import { YTvData } from "@tendai/types";
 export const getYTvData = async (
 	key: string,
 	maxResults?: number
-): Promise<YTvData[]> => {
-	const respons = await fetch(
-		`https://www.googleapis.com/youtube/v3/search?q=${key}&part=snippet&maxResults=${
-			maxResults || 1
-		}&key=${process.env.GOOGLE_API_KEY}`
-	);
-	const json = await respons.json();
+): Promise<YTvData[] | undefined> => {
 	const dataOut: YTvData[] = [];
-	console.log(json.error);
-	json.items.forEach((e: any) => {
-		if (!(e.id.kind === "youtube#video")) return;
-		dataOut.push({
-			id: e.id.videoId,
-			title: e.snippet.title,
-			channelTitle: e.snippet.channelTitle,
-			publishTime: e.snippet.publishTime,
-			thumbnail: e.snippet.thumbnails.default.url,
-		});
-	});
+	try {
+		const respons = await fetch(
+			`https://www.googleapis.com/youtube/v3/search?q=${key}&part=snippet&maxResults=${
+				maxResults || 1
+			}&key=${process.env.GOOGLE_API_KEY}`
+		);
+		const json = await respons.json();
 
+		if (!json.items) return;
+		json.items.forEach((e: any) => {
+			if (!(e.id.kind === "youtube#video")) return;
+			dataOut.push({
+				id: e.id.videoId,
+				title: e.snippet.title,
+				channelTitle: e.snippet.channelTitle,
+				publishTime: e.snippet.publishTime,
+				thumbnail: e.snippet.thumbnails.default.url,
+			});
+		});
+	} catch (error) {
+		console.error(error);
+	}
 	return dataOut;
 };
